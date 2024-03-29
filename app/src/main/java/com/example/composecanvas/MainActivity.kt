4e81e16f220c320e1fe607f02199ecf991eb6664
Niranjan V <rathinamniranjan@gmail.com>
2024-03-29 15:33:07 +0530

    Chess Board Splash Screen

--- app/src/main/java/com/example/composecanvas/MainActivity.kt
+++ app/src/main/java/com/example/composecanvas/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composecanvas
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -7,7 +8,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
@@ -19,12 +25,17 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Column
+
+
 import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.Spacer
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
@@ -34,19 +45,33 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
@@ -65,8 +90,10 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 //
-                    ChessBoard()
+                    ChessBoardSplash()
+
                 }
+
             }
         }
     }
@@ -85,9 +112,9 @@ fun DrawCircle(size:Dp) {
     }
 }
 
-//@Preview(showBackground = true, showSystemUi = true)
-//@Composable
-//fun CanvasPreview() {
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ChessBoardCanvasPreview() {
 //
 ////    ChessBoard()
 //    Column(modifier = Modifier
@@ -110,13 +137,13 @@ fun DrawCircle(size:Dp) {
 //
 //
 //    }
+    ChessBoardCanvas()
 
-
-//}
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun CanvasPreview() {
+fun ChessBoardPreview() {
 
     ChessBoard()
 }
@@ -219,3 +246,68 @@ fun Square(color: Color, size: Dp, pieceIcon: Int? = null,onClick:(piece:Int)->U
 
     }
 }
+
+@Composable
+fun ChessBoardCanvas(degrees:Float = 0f) {
+
+    Box(contentAlignment = Alignment.Center,modifier = Modifier.size(500.dp)){
+        Canvas(modifier = Modifier
+            .size(400.dp)
+            .padding(40.dp)
+
+        ){
+            rotate(degrees = 45f){
+                repeat(8){row->
+                    repeat(8){column->
+                        val color = if((row+column) %2 == 0) Color.Blue else Color.Black
+                        drawRect(color = color, topLeft = Offset(degrees*row,degrees*column), size = Size(40f,40f))
+                    }
+                }
+
+            }
+
+            val text = "Chess Board"
+            val textWidth = size.width / 2
+            val textHeight = size.height / 2
+            drawIntoCanvas {
+                it.nativeCanvas.drawText(
+                    text,
+                    textWidth,
+                    textHeight,
+                    Paint().apply {
+                        color = Color.Black.toArgb() // Adjust text color
+                        textSize = 100f // Adjust text size
+                        textAlign = Paint.Align.CENTER
+                    }
+                )
+            }
+            }
+        }
+
+    }
+
+@Composable
+fun ChessBoardSplash() {
+    val degrees = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+    LaunchedEffect(key1 = true){
+        degrees.animateTo(40f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+
+    }
+
+
+    ChessBoardCanvas(degrees.value)
+
+}
+
+
+
+
+
+
